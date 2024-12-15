1f8b37ff2d23f39077c24aea49755bb18f9f82a6
unknown <adrianapaivausa@icloud.com>
2024-12-15 12:53:53 -0500

    fixing about us spacing

--- src/components/AboutUs.tsx
+++ src/components/AboutUs.tsx
@@ -25,7 +25,7 @@ const AboutUs: React.FC = () => {
         />
 
         <div className="relative">
-          <div className="relative z-1 flex items-center h-[45rem] mb-5 p-8 border border-n-1/10 rounded-3xl overflow-hidden lg:p-20 xl:h-[46rem]">
+          <div className="relative z-1 flex items-center h-[50rem] md:h-[45rem] mb-5 p-8 border border-n-1/10 rounded-3xl overflow-hidden lg:p-20 xl:h-[46rem]">
             <div className="absolute top-0 left-0 w-full h-full pointer-events-none md:w-3/5 xl:w-auto">
               <img
                 className="w-full h-full object-cover md:object-right md:opacity-30 opacity-30 xl:opacity-100"
